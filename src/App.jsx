d8cbfd065bfe2f461bc9f1be9532e6a7f9476512
megbug <meghub@proton.me>
2023-08-23 22:36:06 +0200

    set up pre configured files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom'; 
+
+import LandingPage from './LandingPage'
+
 import './App.css'
 
 function App() {
-
 	return (
     	<>
+			<Routes>
+				<Route path='/' element={<LandingPage/>}/>
+			</Routes>
     	</>
 	)
 }
